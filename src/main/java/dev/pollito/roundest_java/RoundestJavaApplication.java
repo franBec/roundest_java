1b718afc80bff091f671f7e29ed76246ca6a38c8
Fran Bec <franbecvort@gmail.com>
2024-11-28 10:45:04 +0000

    initial commit

---
+++ src/main/java/dev/pollito/roundest_java/RoundestJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.pollito.roundest_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoundestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pollito/roundest_java/RoundestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pollito.roundest_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
