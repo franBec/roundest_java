a708ddfe054681d15d23964ff6be20a6275fe9b3
Fran Bec <franbecvort@gmail.com>
2024-12-12 18:25:25 +0000

    sort is now a List<String>

--- src/main/java/dev/pollito/roundest_java/controller/advice/GlobalControllerAdvice.java
+++ src/main/java/dev/pollito/roundest_java/controller/advice/GlobalControllerAdvice.java
@@ -11,7 +11,6 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
